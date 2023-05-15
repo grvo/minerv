9e8a1eeeba85104092fa567baa5671f293129437
furstamp <gyket.pentest.133971@protonmail.com>
2023-05-15 16:30:28 -0300

    implementação de utilidade de api

---
+++ app/assets/javascripts/api/api_utils.js
@@ -0,0 +1,6 @@
+// imports
+import { joinPaths } from '../lib/utils/url_utility';
+
+export function buildApiUrl(url) {
+    return joinPaths('/', gon.relative_url_root || '', url.replace(':version', gon.api_version));
+}
