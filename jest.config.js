116306d8c6b61f93725fd06f1d77826259b51722
furstamp <gyket.pentest.133971@protonmail.com>
2023-05-16 21:44:08 -0300

    primeira etapa de configuração de jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// consts
+const fs = require('fs');
+
+// consts locais
+// todo: const IS_JH = require('./config/helpers/is_jh_env');
+// todo: const baseConfig = require('./jest.config.base');
+
+// todo: remover existssync ao passo que jh adiciona jest.config.js
