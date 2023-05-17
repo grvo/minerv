597affa148f7233243d6ae6a385b6847e21059c1
furstamp <gyket.pentest.133971@protonmail.com>
2023-05-16 21:51:12 -0300

    ambiente jh agora na configuração de jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,17 @@
 const fs = require('fs');
 
 // consts locais
-// todo: const IS_JH = require('./config/helpers/is_jh_env');
+const IS_JH = require('./config/helpers/is_jh_env');
 // todo: const baseConfig = require('./jest.config.base');
 
 // todo: remover existssync ao passo que jh adiciona jest.config.js
+if (IS_JH && fs.existsSync('./jh/jest.config.js')) {
+    // não explicitar com regras eslint-disable
+    
+    // eslint-disable-next-line
+    module.exports = require('./jh/jest.config');
+} else {
+    module.exports = {
+        ...baseConfig('spec/frontend')
+    };
+}
