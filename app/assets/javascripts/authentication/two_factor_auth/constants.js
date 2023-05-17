ebb6e8b325b2ea6c7d2ddb15dc2eb5a28cea0021
grivo <90540827+grvo@users.noreply.github.com>
2023-05-17 11:55:25 -0300

    implementação de constantes para 2fa

---
+++ app/assets/javascripts/authentication/two_factor_auth/constants.js
@@ -0,0 +1,19 @@
+// consts
+
+// botões de ações
+export const COPY_BUTTON_ACTION = 'copy';
+export const DOWNLOAD_BUTTON_ACTION = 'download';
+export const PRINT_BUTTON_ACTION = 'print';
+
+// prefixo de tracking label
+export const TRACKING_LABEL_PREFIX = '2fa_recovery_codes_';
+
+// recuperação de código (download de filename)
+export const RECOVERY_CODE_DOWNLOAD_FILENAME = 'gitlab-recovery-codes.txt';
+
+// parâmetro de query de sucesso
+export const SUCCESS_QUERY_PARAM = 'two_factor_auth_enabled_successfully';
+
+// atalho do teclado de -> copiar
+// cmd + c / ctrl + c / mod + c
+export const COPY_KEYBOARD_SHORTCUT = 'mod+c';
