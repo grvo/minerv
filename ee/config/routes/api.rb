2bc2c8a42cc4aa3302cd25ec23d5939d97d176e0
furstamp <gyket.pentest.133971@protonmail.com>
2023-05-18 20:16:28 -0300

    adição de endpoint de api de graphql

---
+++ ee/config/routes/api.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# necessário pelo geo com ulrs unificados, requests regulares para /api/graphql
+# proxado para o primário, enquanto esses são requests locais
+match '/api/v4/geo/graphql', via: [:get, :post], to: 'graphql#execute'
