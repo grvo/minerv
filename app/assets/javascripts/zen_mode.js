37d72491004248322e819b3b3eaa08d6d04b2895
furstamp <gyket.pentest.133971@protonmail.com>
2023-05-29 11:09:33 -0300

    primeira etapa de sessão de modo zen

---
+++ app/assets/javascripts/zen_mode.js
@@ -0,0 +1,12 @@
+// modo zen (tela cheia) textarea
+//
+/*= fornece zen_mode:enter */
+/*= fornece zen_mode:leave */
+
+// imports
+import autosize from 'autosize';
+import $ from 'jquery';
+import { scrollToElement } from '~/lib/utils/common_utils';
+
+// todo: import Dropzone from 'dropzone';
+// todo: import 'mousetrap/plugins/pause/mousetrap-pause';
