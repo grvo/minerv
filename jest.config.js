8272e57b6768ac0fff065654a44bc7868822e5ed
furstamp <gyket.pentest.133971@protonmail.com>
2023-06-01 20:49:58 -0300

    constando configuração base ao arquivo de configuração do jest

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 
 // consts locais
 const IS_JH = require('./config/helpers/is_jh_env');
-// todo: const baseConfig = require('./jest.config.base');
+const baseConfig = require('./jest.config.base');
 
 // todo: remover existssync ao passo que jh adiciona jest.config.js
 if (IS_JH && fs.existsSync('./jh/jest.config.js')) {
     // não explicitar com regras eslint-disable
-    
+
     // eslint-disable-next-line
     module.exports = require('./jh/jest.config');
 } else {
