4b0b630413c67ebcd8dea2b75e3a6384e8446956
furstamp <gyket.pentest.133971@protonmail.com>
2023-06-02 19:20:16 -0300

    remoção de método loadhtmlfixture

---
+++ spec/frontend/__helpers__/fixtures.js
@@ -0,0 +1,9 @@
+// exports
+export const resetHTMLFixture = () => {
+    document.head.innerHTML = '';
+    document.body.innerHTML = '';
+}
+
+export const setHTMLFixture = (htmlContent) => {
+    document.body.innerHTML = htmlContent;
+};
