cdd57c35ea580f76d6b7348b6ae0ea1a3e7152bf
furstamp <gyket.pentest.133971@protonmail.com>
2023-06-02 18:14:36 -0300

    adição de link para input de spam no forms de report

---
+++ app/assets/javascripts/abuse_reports/components/links_to_spam_input.vue
@@ -0,0 +1,95 @@
+<script>
+// imports de dependências
+import {
+    GlButton,
+    GlFormGroup,
+    GlFormInput
+} from '@gitlab/ui';
+
+// imports locais
+import {
+    s__
+} from '~/locale';
+
+// exports
+export default {
+    name: 'LinksToSpamInput',
+
+    components: {
+        GlButton,
+        GlFormGroup,
+        GlFormInput
+    },
+
+    i18n: {
+        label: s__('ReportAbuse|link para spam'),
+
+        description: s__('ReportAbuse|url desse usuário postando spam'),
+
+        addAnotherText: s__('ReportAbuse|adicionar outro link')
+    },
+
+    props: {
+        previousLinks: {
+            type: Array,
+            required: false,
+
+            default: () => []
+        }
+    },
+
+    data() {
+        return {
+            links: this.previousLinks.length > 0 ? this.previousLinks : ['']
+        };
+    },
+
+    methods: {
+        addAnotherInput() {
+            this.links.push('');
+        }
+    }
+};
+</script>
+
+<template>
+    <div>
+        <template v-for="(link, index) in links">
+            <div :key="index" class="row">
+                <div class="col-lg-8">
+                    <gl-form-group class="gl-mt-5">
+                        <template #label>
+                            <div class="gl-pb-2">
+                                {{ $options.i18n.label }}
+                            </div>
+
+                            <div class="gl-font-weight-normal">
+                                {{ $options.i18n.description }}
+                            </div>
+                        </template>
+
+                        <gl-form-input
+                            v-model.trim="links[index]"
+                            type="url"
+                            name="abuse_report[links_to_spam][]"
+                            autocomplete="off"
+                        />
+                    </gl-form-group>
+                </div>
+            </div>
+        </template>
+
+        <div class="row">
+            <div class="col-lg-8">
+                <gl-button
+                    variant="link"
+                    icon="plus"
+                    class="gl-float-right"
+                    @click="addAnotherInput"
+                >
+                    {{ $options.i18n.addAnotherText }}
+                </gl-button>
+            </div>
+        </div>
+    </div>
+</template>
