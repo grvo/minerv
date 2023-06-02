a87e243ed494f9d0c53cb67bb107dd678b38a00f
furstamp <gyket.pentest.133971@protonmail.com>
2023-06-02 19:25:11 -0300

    re-exportação de exports nomeados do vue

---
+++ spec/frontend/__mocks__/vue/index.js
@@ -0,0 +1,10 @@
+// imports de dependências
+import Vue from 'vue';
+
+// configuração de vue
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+
+// exports de vue
+export default Vue;
+export * from 'vue';
