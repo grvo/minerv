f8455832e42db1f5d0640febe7aebb9de21a92cc
furstamp <gyket.pentest.133971@protonmail.com>
2023-06-02 18:41:08 -0300

    partial _gl_toggle para viewcomponent

---
+++ app/components/pajamas/toggle_component.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+# renderizar elemento root gltoggle
+# para atualmente incializar componente, certificar-se de chamar o helper inittoggle de ~/toggles
+class Pajamas::ToggleComponent < Pajamas::Component
+    LABEL_POSITION_OPTIONS = [:top, :left, :hidden].freeze
+
+    # @param [String] classes
+    # @param [String] label
+    
+    # @param [Symbol] label_position :top, :left or :hidden
+
+    # @param [String] id
+    # @param [String] name
+    # @param [String] help
+
+    # @param [Hash] data
+
+    # @param [Boolean] is_disabled
+    # @param [Boolean] is_checked
+    # @param [Boolean] is_loading
+    def initialize(
+        classes:,
+        label: nil,
+        label_position: nil,
+
+        id: nil,
+        name: nil,
+        help: nil,
+        data: {},
+
+        is_disabled: false,
+        is_checked: false,
+        is_loading: false
+    )
+        @id = id
+        @name = name
+
+        @classes = classes
+        @label = label
+
+        @label_position = filter_attribute(
+            label_position,
+
+            LABEL_POSITION_OPTIONS
+        )
+
+        @help = help
+        @data = data
+
+        @is_disabled = is_disabled
+        @is_checked = is_checked
+        @is_loading = is_loading
+    end
+end
