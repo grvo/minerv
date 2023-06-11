ff0c011bc2ca2268ac588d90d79df3493e8fe893
furstamp <gyket.pentest.133971@protonmail.com>
2023-06-10 23:54:42 -0300

    deleção de algumas propriedades de itens de trabalho

---
+++ app/assets/javascripts/work_items/utils.js
@@ -0,0 +1,83 @@
+// import de dependência
+import {
+    uniqueId
+} from 'lodash';
+
+// import local
+import {
+    WIDGET_TYPE_HIERARCHY,
+    WIDGET_TYPE_CURRENT_USER_TODOS,
+
+    CURRENT_USER_TODOS_TYPENAME,
+
+    TODO_CONNECTION_TYPENAME,
+    TODO_EDGE_TYPENAME,
+    TODO_TYPENAME,
+
+    WORK_ITEM_TYPENAME,
+    WORK_ITEM_UPDATE_PAYLOAD_TYPENAME
+} from '~/work_items/constants';
+
+export const findHierarchyWidgets = (widgets) => widgets?.find((widget) => widget.type === WIDGET_TYPE_HIERARCHY);
+export const findHierarchyWidgetChildren = (workItem) => findHierarchyWidgets(workItem?.widgets)?.children.nodes;
+
+const autocompleteSourcesPath = (autocompleteType, fullPath, workItemIid) => {
+    return `${
+        gon.relative_url_root || ''
+    }/${fullPath}/-/autocomplete_sources/${autocompleteType}?type=WorkItem&type_id=${workItemIid}`;
+};
+
+export const autocompleteDataSources = (fullPath, iid) => ({
+    labels: autocompleteSourcesPath('labels', fullPath, iid),
+    members: autocompleteSourcesPath('members', fullPath, iid),
+    commands: autocompleteSourcesPath('commands', fullPath, iid)
+});
+
+export const markdownPreviewPath = (fullPath, iid) =>
+    `${
+        gon.relative_url_root || ''
+    }/${fullPath}/preview_markdown?target_type=WorkItem&target_id=${iid}`;
+
+export const getWorkItemTodoOptimisticResponse = ({ workItem, pendingTodo }) => {
+    const todo = pendingTodo
+        ? [
+            {
+                node: {
+                    id: -uniqueId(),
+                    state: 'pending',
+
+                    __typename: TODO_TYPENAME
+                },
+
+                __typename: TODO_EDGE_TYPENAME
+            }
+        ] : [];
+
+    return {
+        workItemUpdate: {
+            errors: [],
+
+            workItem: {
+                ...workItem,
+
+                widgets: [
+                    {
+                        type: WIDGET_TYPE_CURRENT_USER_TODOS,
+
+                        currentUserTodos: {
+                            edges: todo,
+
+                            __typename: TODO_CONNECTION_TYPENAME
+                        },
+
+                        __typename: CURRENT_USER_TODOS_TYPENAME
+                    }
+                ],
+
+                __typename: WORK_ITEM_TYPENAME
+            },
+
+            __typename: WORK_ITEM_UPDATE_PAYLOAD_TYPENAME
+        }
+    };
+};
