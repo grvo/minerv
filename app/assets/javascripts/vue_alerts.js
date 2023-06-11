b35d1da87b257bb51e926fbfe21c6f0126a274e8
furstamp <gyket.pentest.133971@protonmail.com>
2023-06-10 22:48:23 -0300

    ativação de atributo seguro para cookies frontend

---
+++ app/assets/javascripts/vue_alerts.js
@@ -0,0 +1,63 @@
+// import de dependência
+import Vue from 'vue';
+
+// imports locais
+import DismissibleAlert from '~/vue_shared/components/dismissible_alert.vue';
+
+import {
+    setCookie,
+    parseBoolean
+} from '~/lib/utils/common_utils';
+
+const getCookieExpirationPeriod = (expirationPeriod) => {
+    const defaultExpirationPeriod = 30;
+    const alertExpirationPeriod = Number(expirationPeriod);
+
+    return !expirationPeriod || Number.isNaN(alertExpirationPeriod)
+        ? defaultExpirationPeriod
+        : alertExpirationPeriod;
+};
+
+const mountVueAlert = (el) => {
+    const props = {
+        html: el.innerHTML
+    };
+
+    const attrs = {
+        ...el.dataset,
+
+        dismissible: parseBoolean(el.dataset.dismissible)
+    };
+
+    const {
+        dismissCookieName,
+        dismissCookieExpire
+    } = el.dataset;
+
+    return new Vue({
+        el,
+
+        render(createElement) {
+            return createElement(DismissibleAlert, {
+                props,
+                attrs,
+
+                on: {
+                    alertDismissed() {
+                        if (!dismissCookieName) {
+                            return;
+                        }
+
+                        setCookie(dismissCookieName, true, {
+                            expires: getCookieExpirationPeriod(dismissCookieExpire)
+                        });
+                    }
+                }
+            });
+        }
+    });
+};
+
+export default () => [
+    ...document.querySelectorAll('.js-vue-alert')
+].map(mountVueAlert);
