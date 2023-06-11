47de3b67c091f72d4b0c8f280b049cd6e9d256ea
furstamp <gyket.pentest.133971@protonmail.com>
2023-06-10 23:50:12 -0300

    deleção de propriedades fetchbyiid

---
+++ app/assets/javascripts/work_items/pages/create_work_item.vue
@@ -0,0 +1,235 @@
+<script>
+// import de dependência
+import {
+    GlButton,
+    GlAlert,
+    GlLoadingIcon,
+    GlFormSelect
+} from '@gitlab/ui';
+
+// imports locais
+import createWorkItemMutation from '../graphql/create_work_item.mutation.graphql';
+import projectWorkItemTypesQuery from '../graphql/project_work_item_types.query.graphql';
+import workItemByIidQuery from '../graphql/work_item_by_iid.query.graphql';
+
+import {
+    TYPENAME_PROJECT
+} from '~/graphql_shared/constants';
+
+import {
+    getPreferredLocales,
+    s__
+} from '~/locale';
+
+import {
+    capitalizeFirstCharacter
+} from '~/lib/utils/text_utility';
+
+import {
+    sprintfWorkItem,
+    I18N_WORK_ITEM_ERROR_CREATING
+} from '../constants';
+
+// import
+import ItemTitle from '../components/item_title.vue';
+
+export default {
+    fetchTypesErrorText: s__(
+        'WorkItem|algo deu errado ao buscar tipos de item de trabalho. por favor, tente novamente'
+    ),
+
+    components: {
+        GlButton,
+        GlAlert,
+        GlLoadingIcon,
+        ItemTitle,
+        GlFormSelect
+    },
+
+    inject: ['fullPath'],
+
+    props: {
+        initialTitle: {
+            type: String,
+            required: false,
+            default: ''
+        }
+    },
+
+    data() {
+        return {
+            title: this.initialTitle,
+            error: null,
+            workItemTypes: [],
+            selectedWorkItemType: null,
+            loading: false
+        };
+    },
+
+    apollo: {
+        workItemTypes: {
+            query: projectWorkItemTypesQuery,
+
+            variables() {
+                return {
+                    fullPath: this.fullPath
+                };
+            },
+
+            update(data) {
+                return data.workspace?.workItemTypes?.nodes.map((node) => ({
+                    value: node.id,
+
+                    text: capitalizeFirstCharacter(node.name.toLocaleLowerCase(getPreferredLocales()[0]))
+                }));
+            },
+
+            error() {
+                this.error = this.$options.fetchTypesErrorText;
+            }
+        }
+    },
+
+    computed: {
+        formOptions() {
+            return [
+                {
+                    value: null,
+                    text: s__('WorkItem|selecionar tipo')
+                }, ...this.workItemTypes
+            ];
+        },
+
+        isButtonDisabled() {
+            return this.title.trim().length === 0 || !this.selectedWorkItemType;
+        },
+
+        createErrorText() {
+            const workItemType = this.workItemTypes.find(
+                (item) => item.value === this.selectedWorkItemType
+            )?.text;
+
+            return sprintfWorkItem(I18N_WORK_ITEM_ERROR_CREATING, workItemType);
+        }
+    },
+
+    methods: {
+        async createWorkItem() {
+            this.loading = true;
+
+            await this.createStandaloneWorkItem();
+
+            this.loading = false;
+        },
+
+        async createStandaloneWorkItem() {
+            try {
+                const response = await this.$apollo.mutate({
+                    mutation: createWorkItemMutation,
+
+                    variables: {
+                        input: {
+                            title: this.title,
+                            projectPath: this.fullPath,
+                            workItemTypeId: this.selectedWorkItemType
+                        }
+                    },
+
+                    update: (store, { data: { workItemCreate } }) => {
+                        const { workItem } = workItemCreate;
+
+                        store.writeQuery({
+                            query: workItemByIidQuery,
+
+                            variables: {
+                                fullPath: this.fullPath,
+
+                                iid: workItem.iid
+                            },
+
+                            data: {
+                                workspace: {
+                                    __typename: TYPENAME_PROJECT,
+
+                                    id: workItem.project.id,
+
+                                    workItems: {
+                                        __typename: 'WorkItemConnection',
+
+                                        nodes: [workItem]
+                                    }
+                                }
+                            }
+                        });
+                    }
+                });
+
+                this.$router.push({
+                    name: 'workItem',
+
+                    params: {
+                        id: response.data.workItemCreate.workItem.iid
+                    }
+                });
+            } catch {
+                this.error = this.createErrorText;
+            }
+        },
+
+        handleTitleInput(title) {
+            this.title = title;
+        },
+
+        handleCancelClick() {
+            this.$router.go(-1);
+        }
+    }
+};
+</script>
+
+<template>
+    <form @submit.prevent="createWorkItem">
+        <gl-alert v-if="error" variant="danger" @dismiss="error = null">{{ error }}</gl-alert>
+
+        <div data-testid="content">
+            <item-title :title="initialTitle" data-testid="title-input" @title-input="handleTitleInput" />
+
+            <div>
+                <gl-loading-icon
+                    v-if="$apollo.queries.workItemTypes.loading"
+                    size="lg"
+                    data-testid="loading-types"
+                />
+
+                <gl-form-select
+                    v-else
+                    v-model="selectedWorkItemType"
+                    :options="formOptions"
+                    class="gl-max-w-26"
+                />
+            </div>
+        </div>
+
+        <div class="gl-bg-gray-10 gl-py-5 gl-px-6 gl-mt-4">
+            <gl-button
+                variant="confirm"
+                :disabled="isButtonDisabled"
+                class="gl-mr-3"
+                :loading="loading"
+                data-testid="create-button"
+                type="submit"
+            >
+                {{ s__('WorkItem|criar item de trabalho') }}
+            </gl-button>
+
+            <gl-button
+                type="button"
+                data-testid="cancel-button"
+                class="gl-order-n1"
+                @click="handleCancelClick"
+            >
+                {{ __('cancelar') }}
+            </gl-button>
+        </div>
+    </form>
+</template>
