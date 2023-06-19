f3adc2e0b58cd41439d56342faff3dd8369ab71b
grivo <90540827+grvo@users.noreply.github.com>
2023-06-19 11:47:30 -0300

    adição de script para capturar os cliques em rf

---
+++ ee/app/assets/javascripts/registration_features_discovery_message.js
@@ -0,0 +1,20 @@
+// import local
+import api from '~/api';
+
+// exports de serviços de ping
+export const SERVICE_PING_SETTINGS_CLICK_EVENT = 'users_clicking_registration_features_offer';
+export const SERVICE_PING_SETTINGS_LINK_SELECTOR = '.js-go-to-service-ping-settings';
+
+export function initServicePingSettingsClickTracking() {
+    const triggers = document.querySelectorAll(SERVICE_PING_SETTINGS_LINK_SELECTOR);
+
+    if (!triggers.length) {
+        return;
+    }
+
+    triggers.forEach((trigger) => {
+        trigger.addEventListener('click', () => {
+            api.trackRedisCounterEvent(SERVICE_PING_SETTINGS_CLICK_EVENT);
+        });
+    });
+}
