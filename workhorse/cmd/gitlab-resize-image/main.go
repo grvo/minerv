4ac461b7aee06433364663829aebe4cf9e806ce3
grivo <90540827+grvo@users.noreply.github.com>
2023-06-23 09:32:36 -0300

    renomeação de módulo para gitlab/workhorse

---
+++ workhorse/cmd/gitlab-resize-image/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"os"
+	"strconv"
+
+	"github.com/disintegration/imaging"
+
+	"gitlab.com/gitlab-org/gitlab/workhorse/cmd/gitlab-resize-image/png"
+)
+
+func main() {
+	if err := _main(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: fatal: %v\n", os.Args[0], err)
+
+		os.Exit(1)
+	}
+}
+
+func _main() error {
+	widthParam := os.Getenv("GL_RESIZE_IMAGE_WIDTH")
+
+	requestedWidth, err := strconv.Atoi(widthParam)
+
+	if err != nil {
+		return fmt.Errorf("GL_RESIZE_IMAGE_WIDTH: %w", err)
+	}
+
+	pngReader, err := png.NewReader(os.Stdin)
+
+	if err != nil {
+		return fmt.Errorf("construir leitor de png: %w", err)
+	}
+
+	src, formatName, err := image.Decode(pngReader)
+
+	if err != nil {
+		return fmt.Errorf("decodar: %w", err)
+	}
+	
+	imagingFormat, err := imaging.FormatFromExtension(formatName)
+	
+	if err != nil {
+		return fmt.Errorf("encontrar formato de imagem: %w", err)
+	}
+
+	image := imaging.Resize(src, requestedWidth, 0, imaging.Lanczos)
+	
+	return imaging.Encode(os.Stdout, image, imagingFormat)
+}
